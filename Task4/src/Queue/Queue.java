8b6b94a34331f9f23ae4970cf30b59fcaa61030d
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2021-09-21 11:32:13 +0200

    Adding structure

---
+++ Task4/src/Queue/Queue.java
@@ -0,0 +1,33 @@
+package Queue;
+
+import Interfaces.IQueue;
+import java.util.Iterator;
+
+public class Queue<E> implements IQueue<E> {
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void enqueue(E element) {
+
+    }
+
+    public E dequeue() {
+
+    }
+
+    public E first() {
+
+    }
+
+    public E last() {
+
+    }
+
+    public Iterator<E> iterator() {
+    }
+}
